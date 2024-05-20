7582ba940be64c1ae2b68873c78b59ea5fc08657
rqbanerjee <rqbanerjee@gmail.com>
2024-05-19 22:02:29 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # paperless-sync
-A script that helps selectively sync files into paperless' consume folder. In Ruby
+A script that helps selectively sync files into paperless' consume folder. 
+
+Written in Ruby
+
+It will:
+1. Look up when this script last ran. It stores a timestamp in a txt file.
+2. Find all files in the File Path specified that have been modifiend since the timestamp
+3. SCP each of those files to the consume folder on the paperless server.
+
+## Usage
+1. Edit the .rb file to specify your own values:
+
+>  Configurable Values:
+  
+>  FILE_PATH='[Path to the local files to sync]'
+
+>  TIMESTAMP_PATH='[script will create a txt file at this writable path]'
+
+>  FILE_TYPES = [ "*.pdf","*.doc","*.docx","*.xls","*.xlsx","*.txt"]
+
+>  SCP_USER = 'paperless'
+
+>  SCP_HOST = '192.168.1.199'
+
+>  SCP_REMOTE_PATH = '/opt/paperless/consume/'
+
+2. If not already setup, use [ssh-copy-id](https://www.ssh.com/academy/ssh/copy-id)  to copy your SSH key to the paperless host.
+3. Run the script like: *ruby sync_to_paperless.rb*
+
+### Dependencies
+
+* Ruby 2.7*+
+* SCP
+* Ability to SCP into paperless host. Passwordless SSH usage is very useful here.
---
+++ sync_to_paperless.rb
@@ -0,0 +1,70 @@
+require 'time'
+
+FILE_PATH='/Volumes/SharedFiles/'
+TIMESTAMP_PATH='/home/rqbanerjee/paperless/last_run_timestamp.txt'
+FILE_TYPES = [ "*.pdf","*.doc","*.docx","*.xls","*.xlsx","*.txt"]
+SCP_USER = 'paperless'
+SCP_HOST = '192.168.1.199'
+SCP_REMOTE_PATH = '/opt/paperless/consume/'
+
+def get_all_files
+  all_files = []
+  Dir.chdir(FILE_PATH)
+
+  FILE_TYPES.each do |ft|
+    path = File.join"**", ft
+    new_files = Dir.glob(path)
+    all_files << new_files
+  end
+  all_files.flatten
+end
+
+def scp_files(files)
+  if files.nil? or files.empty?
+    puts "No files needing sync. Doing nothing."
+    return
+  end
+  scp_cmd_suffix = "#{SCP_USER}@#{SCP_HOST}:#{SCP_REMOTE_PATH}"
+  files.each do |f|
+    system ("scp \"#{FILE_PATH}/#{f}\" #{scp_cmd_suffix}")
+  end
+end
+
+def write_timestamp
+  File.open(TIMESTAMP_PATH, "w") { |f| f.write "#{Time.now}\n" }
+  puts "Wrote timestamp to #{TIMESTAMP_PATH}"
+end
+
+def get_last_timestamp
+  timestamp_str = File.read(TIMESTAMP_PATH)
+  timestamp = Time.parse(timestamp_str)
+  puts "Script last ran at #{timestamp}."
+  timestamp
+end
+
+def filter_by_timestamp(all_files, timestamp)
+  filtered = []
+  all_files.each do |f|
+    mod_time = File.stat(f).mtime
+    if mod_time > timestamp
+      filtered << f
+      puts "File #{f} was modified on #{mod_time}, more recent than last run at #{timestamp}. Will scp it."
+    end
+
+  end
+  filtered
+end
+
+begin
+  all_files = get_all_files
+  last_run_timestamp = get_last_timestamp
+  files_to_copy = filter_by_timestamp(all_files, last_run_timestamp)
+
+rescue Exception => e
+    puts "Exception thrown!"
+    puts e.message
+  ensure
+    scp_files(files_to_copy)
+    write_timestamp
+end
+
